92bcd48ff2c5bdd0c2d6d52930a897da7be6f022
Owen Wei lab PC <weiyibo97@gmail.com>
2020-12-24 16:59:13 +0800

    finish 5.16 delay and force

--- Part B/Section5/section5.rkt
+++ Part B/Section5/section5.rkt
@@ -28,3 +28,31 @@
 
 (define sixteen1 (pow1 4 2))
 (define sixteen2 ((pow2 4) 2))
+
+#lang racket
+(provide (all-defined-out))
+
+(define (my-thunk th)
+    (mcons #f th))
+
+(define (my-force p)
+    (if (mcar p)
+        (mcdr p)
+        (begin (set-mcar! p #t)
+                (set-mcdr! p ((mcdr p)))
+                (mcdr p))))
+
+(define (slow-add x y)
+    (letrec ([slow-id (lambda (y z)
+                        (if (= 0 z)
+                            y
+                            (slow-id y (- z 1))))])
+    (+ (slow-id x 500000000) y)))
+
+(define (my-mult x y-thunk)
+    (cond   [(= x 0) 0]
+            [(= x 1) (y-thunk)]
+            [#t (+ (y-thunk) (my-mult (- x 1) y-thunk))]))
+
+(define seven-thunk (lambda () (slow-add 3 4)))
+(define seven-promise (my-thunk seven-thunk))
